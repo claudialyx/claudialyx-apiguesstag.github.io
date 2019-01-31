10060b228d2cbea2bdf91b02b0bddf869e0b525b
claudialyx <46521005+claudialyx@users.noreply.github.com>
2019-01-31 18:23:57 +0800

    API guess tag

--- script.js
+++ script.js
@@ -1,82 +1,83 @@
 // must display these in buttons
-keywords=["sports", "food", "bags", "animals"] 
+keywords = ["sports", "food", "bags", "animals"]
 total = ["sports", "food", "bags", "animals", "plant", "transportation", "oprah", "landscape", "kuala lumpur", "architecture"]
 
 
 
 const list = document.getElementById("list-data")
-const index = Math.floor(Math.random()*total.length)
+const index = Math.floor(Math.random() * total.length)
 const tagName = total[index];
 const button = document.getElementsByClassName("button")
 console.log(tagName)
 const optionsGiven = [tagName];
 
 // to use tumblr API library
-function getTaggedPhotos(tagName){
-fetch('https://api.tumblr.com/v2/tagged?tag=' + tagName + '&api_key=QHZK2YBrUJtI1EnvVQW8LL4aZWo5FWTRZ6CMPFfM1UEBhaCrie')
-    .then(function(response){
-        return response.json(); // convert the raw response into json
-        
-    })
-    .then (function(result){
-        console.log(result); // console log the json so we can view it
-        
-        list.innerHTML = "";
+function getTaggedPhotos(tagName) {
+    fetch('https://api.tumblr.com/v2/tagged?tag=' + tagName + '&api_key=QHZK2YBrUJtI1EnvVQW8LL4aZWo5FWTRZ6CMPFfM1UEBhaCrie')
+        .then(function (response) {
+            return response.json(); // convert the raw response into json
 
-        const items = result.response;
+        })
+        .then(function (result) {
+            console.log(result); // console log the json so we can view it
 
-        for(let i = 0; i < items.length; i++){
-            const item = items[i];
+            list.innerHTML = "";
 
-            if(item.photos != undefined){ //why !=undefined?
-                const altSizes = item.photos[0].alt_sizes
-                const imgSrc = altSizes[altSizes.length-2].url;
+            const items = result.response;
 
-                const img = document.createElement("img");
-                img.src = imgSrc;
+            for (let i = 0; i < items.length; i++) {
+                const item = items[i];
 
-                const li = document.createElement("li");
-                li.appendChild(img);
-                // li.innerHTML = imgSrc;
+                if (item.photos != undefined) { //why !=undefined?
+                    const altSizes = item.photos[0].alt_sizes
+                    const imgSrc = altSizes[altSizes.length - 2].url;
 
-                list.appendChild(li);
+                    const img = document.createElement("img");
+                    img.src = imgSrc;
+
+                    const li = document.createElement("li");
+                    li.appendChild(img);
+                    // li.innerHTML = imgSrc;
+
+                    list.appendChild(li);
+                }
             }
-        }
-    })
+        })
 }
 getTaggedPhotos(tagName)
 setup();
 
 // additional keys to be added
-function setup (){
-    for (let i = 0; optionsGiven.length < 4; i++){
+function setup() {
+    for (let i = 0; optionsGiven.length < 4; i++) {
         let tagToAdd = tagName;
-        while (optionsGiven.includes(tagToAdd)){
-            tagToAdd = total[Math.floor(Math.random()*total.length)]
+        while (optionsGiven.includes(tagToAdd)) {
+            tagToAdd = total[Math.floor(Math.random() * total.length)]
         }
         optionsGiven.push(tagToAdd);
     }
 
     // to sort the options
     optionsGiven.sort(
-        function() { 
-            return 0.5 - Math.random() 
+        function () {
+            return 0.5 - Math.random()
         }
     )
     // to make button clickable
-    for(let i = 0; i < optionsGiven.length; i++){
+    for (let i = 0; i < optionsGiven.length; i++) {
         button[i].innerHTML = optionsGiven[i];
-        button[i].onclick = function(event){ 
-            const buttonClicked = event.target 
+        button[i].onclick = function (event) {
+            const buttonClicked = event.target
             console.log(buttonClicked)
             //check if button clicked matches keyword, if yes - proceed
-            if(button[i].innerHTML != tagName){
-                alert ("The correct answer is " + tagName + " .")
+            if (button[i].innerHTML != tagName) {
+                alert("The correct answer is " + tagName + " .")
                 window.location.reload(true);
             }
-            else if (button[i].innerHTML == tagName){
+            else if (button[i].innerHTML == tagName) {
+                alert("You're right!")
                 window.location.reload(true);
-            
+
             }
         }
     }
